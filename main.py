759e8d8dc85fe62f9055247ba80e4fa9ee22ed43
aditya617p@gmail.com <youremail@example.com>
2024-02-15 17:18:17 +0530

    Added exception handling

--- main.py
+++ main.py
@@ -6,97 +6,111 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
+try:
+    # Setting up the Selenium web driver
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
 
-# Setting up the Selenium web driver
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+    # Opening the webpage
+    driver.get("https://hprera.nic.in/PublicDashboard")
 
-# Opening the webpage
-driver.get("https://hprera.nic.in/PublicDashboard")
-
-# Waiting for the "View Application" links to load
-wait = WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[title="View Application"]')))
+    # Waiting for the "View Application" links to load
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[title="View Application"]')))
 
 # Fetching the first five links from the page
-a_tags = driver.find_elements(By.CSS_SELECTOR, '[title="View Application"]')
-texts_of_a_tags = [a_tag.text for a_tag in a_tags]
+    a_tags = driver.find_elements(By.CSS_SELECTOR, '[title="View Application"]')
+    texts_of_a_tags = [a_tag.text for a_tag in a_tags]
 
 # Removing unwanted text from the list
-texts_of_a_tags = [text for text in texts_of_a_tags if text != 'Previous Detail >>' and text != '']
-a_five = texts_of_a_tags[:5]
+    texts_of_a_tags = [text for text in texts_of_a_tags if text != 'Previous Detail >>' and text != '']
+    a_five = texts_of_a_tags[:5]
 
-def get_details(address):
-    """
-    Retrieves details from a webpage based on the given address.
+    def get_details(address):
+        """
+        Retrieves details from a webpage based on the given address.
 
-    Args:
-        address (str): The address to search for on the webpage.
+        Args:
+            address (str): The address to search for on the webpage.
 
-    Returns:
-        list: A list containing the retrieved details.
-    """
-    output_dict = []
+        Returns:
+            list: A list containing the retrieved details.
+        """
+        output_dict = []
 
-    # Clicking on the link for the given address
-    link_locator = (By.PARTIAL_LINK_TEXT, address)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located(link_locator))
-    link = driver.find_element(*link_locator)
-    link.click()
+        # Clicking on the link for the given address
+        link_locator = (By.PARTIAL_LINK_TEXT, address)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(link_locator))
+        link = driver.find_element(*link_locator)
+        link.click()
 
-    # Waiting for the details to load
-    element_locator = (By.CSS_SELECTOR, "td.fw-600")
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located(element_locator))
+        # Waiting for the details to load
+        element_locator = (By.CSS_SELECTOR, "td.fw-600")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(element_locator))
 
-    # Retrieving the details
-    tr_elements = driver.find_element(*element_locator)
-    output_dict.append(tr_elements.text)
+        # Retrieving the details
+        tr_elements = driver.find_element(*element_locator)
+        output_dict.append(tr_elements.text)
 
-    # Retrieving additional details
-    element_locator2 = (By.CSS_SELECTOR, "span.fw-600")
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located(element_locator2))
-    tr_elements2 = driver.find_elements(*element_locator2)
-    tr_elements2 = tr_elements2[-7:]
+        # Retrieving additional details
+        element_locator2 = (By.CSS_SELECTOR, "span.fw-600")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(element_locator2))
+        tr_elements2 = driver.find_elements(*element_locator2)
+        tr_elements2 = tr_elements2[-7:]
 
-    # Appending the additional details to the output dictionary
-    for i in range(len(tr_elements2)):
-        if tr_elements2[i].text == "":
-            output_dict.append("N/A")
-        else:
-            output_dict.append(tr_elements2[i].text)
+        # Appending the additional details to the output dictionary
+        for i in range(len(tr_elements2)):
+            if tr_elements2[i].text == "":
+                output_dict.append("N/A")
+            else:
+                output_dict.append(tr_elements2[i].text)
 
-    # Closing the details popup
-    button = driver.find_element(By.CLASS_NAME, "close")
-    button.click()
-    time.sleep(5)
+        # Closing the details popup
+        button = driver.find_element(By.CLASS_NAME, "close")
+        button.click()
+        time.sleep(5)
 
-    return output_dict
+        return output_dict
 
-# Retrieving details for all five addresses
-all_files = [get_details(i) for i in a_five]
+    # Retrieving details for all five addresses
+    all_files = [get_details(i) for i in a_five]
 
-# Removing "Yes" from the retrieved details
-for i in all_files:
-    if "Yes" in i:
-        i.remove("Yes")
+    # Removing "Yes" from the retrieved details
+    for i in all_files:
+        if "Yes" in i:
+            i.remove("Yes")
 
-# Quitting the web driver
-driver.quit()
+    # Quitting the web driver
+    driver.quit()
 
-# Creating a list of dictionaries with the required details
-#Here we can verify the pan number and GSTIN number for its validity but for now I am just checking if it is present or not (using regex)
-result_list = [
+
+
+    # Creating a list of dictionaries with the required details
+    #Here we can verify the pan number and GSTIN number for its validity but for now I am just checking if it is present or not (using regex)
+    result_list = [
     {
         "Name": entry[0],
         "Permanent Address": entry[-1],
         "PAN No": next((item for item in entry if item.isalnum() and len(item) == 10), 'N/A'),
         "GSTIN No": next((item for item in entry if item.isalnum() and len(item) == 15), 'N/A')
     }
-    for entry in all_files
-]
+        for entry in all_files
+    ]
 
-# Creating a dataframe from the list of dictionaries
-df = pd.DataFrame(result_list)
+    # Creating a dataframe from the list of dictionaries
+    df = pd.DataFrame(result_list)
 
-# Printing the dataframe
-print(df)
+    # Printing the dataframe
+    print(df)
+    df.to_csv('records.csv')
+
+
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+
+
+finally:
+    if 'driver' in locals():
+        driver.quit()
+
+
